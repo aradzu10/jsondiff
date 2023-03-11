eee0395de23a7e57b9b52ab44baafd1857ff9f62
Arad Zulti <aradzulti at gmail>
2023-03-11 12:56:12 +0200

    Done tests - test same dict simple and recursive, same key with diff simple and recursive, test diff key one dict and recursice, test diff key both ways simple and recursive, complex test with all diffs single every list and multiaple

--- jsondiff_test.py
+++ jsondiff_test.py
@@ -48,6 +48,25 @@ def test_diff_keys():
     assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
 
 
+def test_diff_keys_in_one_dict():
+    dict_a = {
+        "a": 1,
+        "b": 1,
+    }
+    dict_b = {
+        "a": 1,
+    }
+    dict_a_mk, dict_b_mk, value_diff = jsondiff.dict_diff(dict_a, dict_b)
+    assert dict_a_mk == [[["b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert len(dict_b_mk) == 0, "Same keys from b in a. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+    dict_b_mk, dict_a_mk, value_diff = jsondiff.dict_diff(dict_b, dict_a)
+    assert dict_a_mk == [[["b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert len(dict_b_mk) == 0, "Same keys from b in a. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+
 def test_same_keys_diff_value_list():
     dict_a = {
         "a": 1,
@@ -132,3 +151,102 @@ def test_diff_key_value_recursive():
     assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
     assert dict_b_mk == [[["a", "a", "c"], 2]], "Key only in dict b. {dict_b_mk}"
     assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+
+def test_diff_keys_in_one_dict():
+    dict_a = {
+        "a": {
+            "a": {
+                "a": 1,
+                "b": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_b = {
+        "a": {
+            "a": {
+                "a": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_a_mk, dict_b_mk, value_diff = jsondiff.dict_diff(dict_a, dict_b)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert len(dict_b_mk) == 0, "Same keys from b in a. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+    dict_b_mk, dict_a_mk, value_diff = jsondiff.dict_diff(dict_b, dict_a)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert len(dict_b_mk) == 0, "Same keys from b in a. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+
+def test_single_missing_key_and_value_diff():
+    dict_a = {
+        "a": {
+            "a": {
+                "a": 1,
+                "b": 1,
+                "diff": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_b = {
+        "a": {
+            "a": {
+                "a": 1,
+                "c": 2,
+                "diff": 2,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_a_mk, dict_b_mk, value_diff = jsondiff.dict_diff(dict_a, dict_b)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert dict_b_mk == [[["a", "a", "c"], 2]], "Key only in dict b. {dict_b_mk}"
+    assert value_diff == [[["a", "a", "diff"], 1, 2]], "Diffrent value. {value_diff}"
+
+    dict_b_mk, dict_a_mk, value_diff = jsondiff.dict_diff(dict_b, dict_a)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert dict_b_mk == [[["a", "a", "c"], 2]], "Key only in dict b. {dict_b_mk}"
+    assert value_diff == [[["a", "a", "diff"], 2, 1]], "Diffrent value. {value_diff}"
+
+
+def test_complex_missing_key_and_value_diff():
+    dict_a = {
+        "a": {
+            "a": {
+                "a": 1,
+                "b": 1,
+                "diff": 1,
+            },
+            "b": {
+                "a": 1,
+            },
+            "c": {
+                "a": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_b = {
+        "a": {
+            "a": {
+                "a": 1,
+                "c": 2,
+                "diff": [1, 2],
+            },
+            "b": 3,
+            "c": {
+                "a": 2,
+                "c": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_a_mk, dict_b_mk, value_diff = jsondiff.dict_diff(dict_a, dict_b)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert dict_b_mk == [[["a", "a", "c"], 2], [["a", "c", "c"], 1]], "Key only in dict b. {dict_b_mk}"
+    assert value_diff == [[["a", "a", "diff"], 1, [1, 2]], [['a', 'b'], {'a': 1}, 3], [['a', 'c', 'a'], 1, 2]], "Diffrent value. {value_diff}"
