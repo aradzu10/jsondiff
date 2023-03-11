e08feda1e7fa144ed409a6e5bc2c316ab7dfa3aa
Arad Zulti <aradzulti at gmail>
2023-03-11 12:43:26 +0200

    Add diff key recursive test

--- jsondiff_test.py
+++ jsondiff_test.py
@@ -102,3 +102,33 @@ def test_same_keys_diff_value_recursive():
     assert len(dict_a_mk) == 0, "Same keys, shouldn't be any diff. {dict_a_mk}"
     assert len(dict_b_mk) == 0, "Same keys, shouldn't be any diff. {dict_b_mk}"
     assert value_diff == [[["a", "a", "a"], 1, 2]], "The diffrent key. {value_diff}"
+
+
+def test_diff_key_value_recursive():
+    dict_a = {
+        "a": {
+            "a": {
+                "a": 1,
+                "b": 1,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_b = {
+        "a": {
+            "a": {
+                "a": 1,
+                "c": 2,
+            },
+        },
+        "b": [1, 2, 3],
+    }
+    dict_a_mk, dict_b_mk, value_diff = jsondiff.dict_diff(dict_a, dict_b)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert dict_b_mk == [[["a", "a", "c"], 2]], "Key only in dict b. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
+
+    dict_b_mk, dict_a_mk, value_diff = jsondiff.dict_diff(dict_b, dict_a)
+    assert dict_a_mk == [[["a", "a", "b"], 1]], "Key only in dict a. {dict_a_mk}"
+    assert dict_b_mk == [[["a", "a", "c"], 2]], "Key only in dict b. {dict_b_mk}"
+    assert len(value_diff) == 0, "Same values for all same keys. {value_diff}"
